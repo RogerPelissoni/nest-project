982c61164f462d3d84fe46259d37d5ca6c56becf
roger_pelissoni <rogerpelissoni@hotmail.com>
2025-11-20 20:57:54 -0300

    added auto swagger

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -8,28 +8,22 @@ import {
   Delete,
   Query,
 } from '@nestjs/common';
-import { ApiTags, ApiBody, ApiParam, ApiQuery } from '@nestjs/swagger';
 import { UserService } from './user.service';
 import { Prisma } from 'prisma/generated/client';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 
-@ApiTags('user')
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Post()
-  @ApiBody({ type: CreateUserDto })
   create(@Body() createUserDto: CreateUserDto) {
     const prismaData: Prisma.UserCreateInput = { ...createUserDto };
     return this.userService.create(prismaData);
   }
 
   @Get()
-  @ApiQuery({ name: 'skip', required: false, type: Number })
-  @ApiQuery({ name: 'take', required: false, type: Number })
-  @ApiQuery({ name: 'orderBy', required: false, type: String })
   findAll(
     @Query('skip') skip?: number,
     @Query('take') take?: number,
@@ -47,15 +41,12 @@ export class UserController {
   }
 
   @Get(':id')
-  @ApiParam({ name: 'id', description: 'ID do usuário' })
   findOne(@Param('id') id: string) {
     const prismaId: Prisma.UserWhereUniqueInput = { id: Number(id) }; // ← converte para number
     return this.userService.findOne(prismaId);
   }
 
   @Patch(':id')
-  @ApiParam({ name: 'id', description: 'ID do usuário' })
-  @ApiBody({ type: UpdateUserDto })
   update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
     const prismaData: Prisma.UserUpdateInput = { ...updateUserDto };
     const prismaId: Prisma.UserWhereUniqueInput = { id: Number(id) }; // ← converte para number
@@ -63,7 +54,6 @@ export class UserController {
   }
 
   @Delete(':id')
-  @ApiParam({ name: 'id', description: 'ID do usuário' })
   remove(@Param('id') id: string) {
     const prismaId: Prisma.UserWhereUniqueInput = { id: Number(id) }; // ← converte para number
     return this.userService.remove(prismaId);
